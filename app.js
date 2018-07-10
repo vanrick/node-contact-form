40e30093d974f561bc5872ee44c1d700705c4776
Rick Van <x.rckvan@gmail.com>
2018-07-10 13:46:13 -0600

    test2

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ app.post('/send', (req, res) => {
     `;
     // create reusable transporter object using the default SMTP transport
     let transporter = nodemailer.createTransport({
-        host: 'imap.gmail.com',
+        host: 'smtp.gmail.com',
         port: 587,
         secure: false, // true for 465, false for other ports
         auth: {
